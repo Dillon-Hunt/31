5783a6ab6515437ed68e52ff5603e2e97b970959
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-02-27 17:46:49 +1000

    Increased Number of Collectables

--- Scripts/foods.js
+++ Scripts/foods.js
@@ -1,77 +1,123 @@
-window.foods = localStorage.getItem("foods") ? JSON.parse(localStorage.getItem("foods")) : [
-    {
-        name: "Apple",
-        tier: "common",
-        amount: 0,
-        weight: 20
-    },
-    {
-        name: "Orange",
-        tier: "common",
-        amount: 0,
-        weight: 20
-    },
-    {
-        name: "Banana",
-        tier: "common",
-        amount: 0,
-        weight: 20
-    },
-    {
-        name: "Carrot",
-        tier: "common",
-        amount: 0,
-        weight: 20
-    },
-    {
-        name: "Lemon",
-        tier: "uncommon",
-        amount: 0,
-        weight: 15
-    },
-    {
-        name: "Tomato",
-        tier: "uncommon",
-        amount: 0,
-        weight: 15
-    },
-    {
-        name: "Strawberry",
-        tier: "uncommon",
-        amount: 0,
-        weight: 15
-    },
-    {
-        name: "Pear",
-        tier: "uncommon",
-        amount: 0,
-        weight: 15
-    },
-    {
-        name: "Chili",
-        tier: "rare",
-        amount: 0,
-        weight: 10
-    },
-    {
-        name: "Waffle",
-        tier: "legendary",
-        amount: 0,
-        weight: 5
-    },
-    {
-        name: "Cake",
-        tier: "epic",
-        amount: 0,
-        weight: 1
-    },
-    {
-        name: "Muffin",
-        tier: "epic",
-        amount: 0,
-        weight: 1
-    },
-]
+if (localStorage.getItem("foods") && JSON.parse(localStorage.getItem("foods")).length === 19) { // Update when added more items (Will Reset Account Foods)
+    window.foods = JSON.parse(localStorage.getItem("foods"))
+} else {
+    window.foods = [
+        {
+            name: "Apple",
+            tier: "common",
+            amount: 0,
+            weight: 20
+        },
+        {
+            name: "Orange",
+            tier: "common",
+            amount: 0,
+            weight: 20
+        },
+        {
+            name: "Banana",
+            tier: "common",
+            amount: 0,
+            weight: 20
+        },
+        {
+            name: "Carrot",
+            tier: "common",
+            amount: 0,
+            weight: 20
+        },
+        {
+            name: "Pineapple",
+            tier: "common",
+            amount: 0,
+            weight: 20
+        },
+        {
+            name: "Lemon",
+            tier: "uncommon",
+            amount: 0,
+            weight: 15
+        },
+        {
+            name: "Tomato",
+            tier: "uncommon",
+            amount: 0,
+            weight: 15
+        },
+        {
+            name: "Strawberry",
+            tier: "uncommon",
+            amount: 0,
+            weight: 15
+        },
+        {
+            name: "Pear",
+            tier: "uncommon",
+            amount: 0,
+            weight: 15
+        },
+        {
+            name: "Watermelon",
+            tier: "uncommon",
+            amount: 0,
+            weight: 15
+        },
+        {
+            name: "Chili",
+            tier: "rare",
+            amount: 0,
+            weight: 10
+        },
+        {
+            name: "Cherry",
+            tier: "rare",
+            amount: 0,
+            weight: 10
+        },
+        {
+            name: "Avocado",
+            tier: "rare",
+            amount: 0,
+            weight: 10
+        },
+        {
+            name: "Mushroom",
+            tier: "rare",
+            amount: 0,
+            weight: 10
+        },
+        {
+            name: "Waffle",
+            tier: "legendary",
+            amount: 0,
+            weight: 5
+        },
+        {
+            name: "Pretzel",
+            tier: "legendary",
+            amount: 0,
+            weight: 5
+        },
+        {
+            name: "Cake",
+            tier: "epic",
+            amount: 0,
+            weight: 1
+        },
+        {
+            name: "Muffin",
+            tier: "epic",
+            amount: 0,
+            weight: 1
+        },
+        {
+            name: "Bacon",
+            tier: "epic",
+            amount: 0,
+            weight: 1
+        },
+    ]
+}
 
 window.foods.forEach(food => {
     element = document.createElement("div")
