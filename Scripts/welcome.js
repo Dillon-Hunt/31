9d9589e94cbb14fdb8b7b4b3d8059040d81b12b4
Dillon Hunt <90228106+Dillon-Hunt@users.noreply.github.com>
2022-03-10 19:48:16 +1000

    Fix to Signup

--- Scripts/welcome.js
+++ Scripts/welcome.js
@@ -1,3 +1,5 @@
+var waiting = false
+
 window.onload = () => {
     if (localStorage.userId && localStorage.email && localStorage.password) {
         window.location.href = "./index.html"
@@ -34,99 +36,103 @@ document.querySelector(".sign-in-submit").onclick = () => {
 }
 
 document.querySelector(".submit").onclick = async () => {
-    const snapshot = await firebase.firestore().collection('Users').get().then((snapshotData) => {
-        return snapshotData.docs.map(doc => doc.data())
-    })
+    if (!waiting) {
+        waiting = true
+        const snapshot = await firebase.firestore().collection('Users').get().then((snapshotData) => {
+            return snapshotData.docs.map(doc => doc.data())
+        })
 
-    var usernameTaken = false
+        var usernameTaken = false
 
-    snapshot.forEach(user => {
-        if (user.userInfo.username.replaceAll(" ", "").toLowerCase() === document.querySelector(".username").value.replaceAll(" ", "").toLowerCase()) {
-            usernameTaken = true
-        }
-    })
+        snapshot.forEach(user => {
+            if (user.userInfo.username.replaceAll(" ", "").toLowerCase() === document.querySelector(".username").value.replaceAll(" ", "").toLowerCase()) {
+                usernameTaken = true
+            }
+        })
 
-    if (usernameTaken) {
-        alert("Username is already taken, try a different one.")
-    } else {
-        const analytics = firebase.analytics();
-        analytics.logEvent("signup")
-        
-        if (document.querySelector(".email").value !== "" && document.querySelector(".password").value !== "") {
-            userData = {
-                userInfo: {
-                    name: document.querySelector(".name").value,
-                    username: document.querySelector(".username").value,
-                },
-
-                stats: {
-                    level: 0,
-                    points: 0,
-                    games: 0,
-                    wins: 0,
-                    ties: 0,
-                    losses: 0,
-                    streak: 0,
-                    averageWin: 0,
-                    fastestWin: 0,
-                    averageLoss: 0,
-                    fastestLoss: 0,
-                },
-
-                foods: {
-                    apple: 0,
-                    orange: 0,
-                    banana: 0,
-                    carrot: 0,
-                    pineapple: 0,
-                    strawberry: 0,
-                    lemon: 0,
-                    tomato: 0,
-                    pear: 0,
-                    watermelon: 0,
-                    kiwifruit: 0,
-                    pumpkin: 0,
-                    chili: 0,
-                    cherry: 0,
-                    avocado: 0,
-                    mushroom: 0,
-                    toast: 0,
-                    waffle: 0,
-                    pretzel: 0,
-                    taco: 0,
-                    muffin: 0,
-                    pancakes: 0,
-                    cake: 0,
-                    icecream: 0,
-                    bacon: 0
-                }, 
-
-                achievements: {
-                    winStreak: {
-                        name: "Card Hero",
-                        level: 0,
-                        goal: 5,
-                        increment: 5,
-                        value: 0,
-                        text: "Congratulations, you won {GOAL} consecutive games against the computer.",
-                        nextText: "Win {GOAL} consecutive games against the computer.",
-                        image: "Achievement-1"
+        if (usernameTaken) {
+            alert("Username is already taken, try a different one.")
+            waiting = false
+        } else {
+            const analytics = firebase.analytics();
+            analytics.logEvent("signup")
+            
+            if (document.querySelector(".email").value !== "" && document.querySelector(".password").value !== "") {
+                userData = {
+                    userInfo: {
+                        name: document.querySelector(".name").value,
+                        username: document.querySelector(".username").value,
                     },
-                    reachLevel: {
-                        name: "Master of Points",
+
+                    stats: {
                         level: 0,
-                        goal: 5,
-                        increment: 5,
-                        value: 0,
-                        text: "Congratulations, you won {GOAL} consecutive games against the computer.",
-                        nextText: "Win {GOAL} consecutive games against the computer.",
-                        image: "Achievement-2"
+                        points: 0,
+                        games: 0,
+                        wins: 0,
+                        ties: 0,
+                        losses: 0,
+                        streak: 0,
+                        averageWin: 0,
+                        fastestWin: 0,
+                        averageLoss: 0,
+                        fastestLoss: 0,
+                    },
+
+                    foods: {
+                        apple: 0,
+                        orange: 0,
+                        banana: 0,
+                        carrot: 0,
+                        pineapple: 0,
+                        strawberry: 0,
+                        lemon: 0,
+                        tomato: 0,
+                        pear: 0,
+                        watermelon: 0,
+                        kiwifruit: 0,
+                        pumpkin: 0,
+                        chili: 0,
+                        cherry: 0,
+                        avocado: 0,
+                        mushroom: 0,
+                        toast: 0,
+                        waffle: 0,
+                        pretzel: 0,
+                        taco: 0,
+                        muffin: 0,
+                        pancakes: 0,
+                        cake: 0,
+                        icecream: 0,
+                        bacon: 0
+                    }, 
+
+                    achievements: {
+                        winStreak: {
+                            name: "Card Hero",
+                            level: 0,
+                            goal: 5,
+                            increment: 5,
+                            value: 0,
+                            text: "Congratulations, you won {GOAL} consecutive games against the computer.",
+                            nextText: "Win {GOAL} consecutive games against the computer.",
+                            image: "Achievement-1"
+                        },
+                        reachLevel: {
+                            name: "Master of Points",
+                            level: 0,
+                            goal: 5,
+                            increment: 5,
+                            value: 0,
+                            text: "Congratulations, you won {GOAL} consecutive games against the computer.",
+                            nextText: "Win {GOAL} consecutive games against the computer.",
+                            image: "Achievement-2"
+                        }
                     }
                 }
+                await signUp(document.querySelector(".email").value, document.querySelector(".password").value, userData)
+                localStorage.accessible = document.querySelector(".accessability").checked
+                window.location.href = "./tutorial.html"
             }
-            await signUp(document.querySelector(".email").value, document.querySelector(".password").value, userData)
-            localStorage.accessible = document.querySelector(".accessability").checked
-            window.location.href = "./tutorial.html"
         }
     }
 }
